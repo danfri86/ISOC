d4ccadc20857a61da8214c9683a23132109210a6
Alexander Hansson <hejhansson@me.com>
2014-07-05 00:43:02 +0200

    Påbörjat nyhetssida.
    Lite funktionalitet på bli medlem-formuläret också gjort.

--- wp-content/themes/isoc.dev/js/functions.js
+++ wp-content/themes/isoc.dev/js/functions.js
@@ -9,6 +9,28 @@ $(document).ready(function() {
 	var $self = $(this);
 
 
-	$("#checkboxForetag").attr("checked") ? alert("Checked") : alert("Unchecked");
+	// Om man trycker i checkbox under medlem-sidan 
+	// så visar vi resten av formuläret.
+	// Vi lägger även till "required" till fälten
+	// i företagsformuläret om checkboxen är checkad.
+	// Vilket vi inte vill ska göra det jobbigt för oss 
+	// vid validering om man inte tryck i den.
 
+	$('#checkboxForetag').change(function(){
+		$('.medlem-foretag').fadeToggle();
+
+		if($('#checkboxForetag').is(':checked')) {
+		  	$('.medlem-foretag').find('input').prop('required',true);
+		}
+		else {
+			$('.medlem-foretag').find('input').prop('required',false);
+		}
+	});
+
+	// Verifiering av "bli medlem"-formulär
+	// Löser sig ganska lätt med HTML5.
+	$( "#medlemFormular" ).submit(function( event ) {
+		alert("wop!");
+		event.preventDefault();
+	});
 });
