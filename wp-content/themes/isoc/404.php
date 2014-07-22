88499b3fbc44ce2c60e0ff3f484f59e6c17a1262
danfri86 <daniel@df-mac.local>
2014-07-22 14:13:47 +0200

    404 skapad

---
+++ wp-content/themes/isoc/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div class="container mt20 mb50 page-layout" role="main">	
+
+	<div class="box-12">
+		<div class="page-header">
+			<h1>404</h1>
+		</div>
+	
+		<div class="box-12">
+			<p>Sidan du försökte besöka finns inte.</p>
+			<a class="btn border" href="<?php bloginfo('url'); ?>">Till förstasidan</a>
+		</div>
+  </div>
+
+<!-- /container -->
+</div>
+
+<?php get_footer(); ?>
