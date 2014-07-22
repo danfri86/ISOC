2d064928395330bedfb7b23f9916dc873f032d54
danfri86 <daniel@df-mac.local>
2014-07-22 23:08:14 +0200

    Nyhetsbrev
    Skrev att funktionen är ur funktion så ingen blir frustrerad och
    trycker och trycker och trycker.

--- wp-content/themes/isoc/footer.php
+++ wp-content/themes/isoc/footer.php
@@ -33,7 +33,7 @@
 
 		<div class="box-4 float-r">
 			<h3 class="">Nyhetsbrev</h3><p>Snart kommer du här att kunna skriva upp din mail och få ta del av information från oss och internationella Internet Society.</p>
-			<input type="text" disabled placeholder="Din e-mail-adress">
+			<input type="text" disabled placeholder="Tillfälligt ur funktion">
 		</div>
 
 		<div class="box-12">
